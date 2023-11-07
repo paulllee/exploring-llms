22746bcd70e98b8e4966c7aaeb258fd6ada8b926
paulllee <paulllee.md@gmail.com>
2023-11-07 05:15:28 +0000

    remove hugging face

--- paul/sandbox.py
+++ paul/sandbox.py
@@ -1,6 +1,5 @@
 import dotenv
 from langchain.llms.openai import OpenAI
-from langchain.llms.huggingface_hub import HuggingFaceHub
 
 # loads .env into your environment
 dotenv.load_dotenv()
@@ -10,10 +9,3 @@ text: str = "what is langchain?"
 # OpenAI example
 openai_llm: OpenAI = OpenAI(temperature=0.9)
 print(openai_llm(text))
-
-# HuggingFace example
-huggingface_llm: HuggingFaceHub = HuggingFaceHub(
-    repo_id="google/flan-t5-xl",
-    model_kwargs={"temperature": 0.75}
-)
-print(huggingface_llm(text))
